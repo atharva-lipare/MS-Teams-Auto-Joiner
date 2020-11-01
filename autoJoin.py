d404198b553ed62615b2e44f9c6d3015a3191811
Atharva Lipare <atharvaslipare@gmail.com>
2020-11-01 19:52:13 +0530

    🐛 Fixes Issue #2

--- autoJoin.py
+++ autoJoin.py
@@ -1,17 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 from datetime import datetime
 import json
-import os
+TEAMS_URL = 'https://teams.microsoft.com/_#/calendarv2'
 
-sleepDelay = 2      # increase if you have a slow internet connection
-timeOutDelay = 30   # increase if you have a slow internet connection
+sleepDelay = 2  # increase if you have a slow internet connection
+timeOutDelay = 30  # increase if you have a slow internet connection
 
-maxParticipants = curParticipants = 0
+curParticipants = 0
 minParticipants = 10
 
 opt = Options()
@@ -19,23 +17,24 @@ opt.add_argument("--disable-infobars")
 opt.add_argument("start-maximized")
 opt.add_argument("--disable-extensions")
 # Pass the argument 1 to allow and 2 to block
-opt.add_experimental_option("prefs", { \
-    "profile.default_content_setting_values.media_stream_mic": 1, 
-    "profile.default_content_setting_values.media_stream_camera": 1,
-    "profile.default_content_setting_values.notifications": 1 
-})
+opt.add_experimental_option("prefs", {"profile.default_content_setting_values.media_stream_mic": 1,
+                                      "profile.default_content_setting_values.media_stream_camera": 1,
+                                      "profile.default_content_setting_values.notifications": 1
+                                      })
 
-browser = webdriver.Chrome(ChromeDriverManager().install(),options=opt)
+browser = webdriver.Chrome(ChromeDriverManager().install(), options=opt)
 
-def wait_and_find_ele_by_id(id, timeout=timeOutDelay):
+
+def wait_and_find_ele_by_id(html_id, timeout=timeOutDelay):
     sleep(sleepDelay)
     for i in range(timeout):
         try:
-            ele = browser.find_element_by_id(id)
+            ele = browser.find_element_by_id(html_id)
         except:
             sleep(sleepDelay)
         else:
             return ele
+
 
 def wait_and_find_ele_by_link_text(text, timeout=timeOutDelay):
     sleep(sleepDelay)
@@ -47,6 +46,7 @@ def wait_and_find_ele_by_link_text(text, timeout=timeOutDelay):
         else:
             return ele
 
+
 def wait_and_find_element_by_xpath(xpath, timeout=timeOutDelay):
     sleep(sleepDelay)
     for i in range(timeout):
@@ -56,6 +56,7 @@ def wait_and_find_element_by_xpath(xpath, timeout=timeOutDelay):
             sleep(sleepDelay)
         else:
             return ele
+
 
 def wait_and_find_elements_by_xpath(xpath, timeout=timeOutDelay):
     sleep(sleepDelay)
@@ -67,78 +68,84 @@ def wait_and_find_elements_by_xpath(xpath, timeout=timeOutDelay):
         else:
             return ele
 
-def checkAndJoinMeeting():
-    global maxParticipants, curParticipants
-    joins = wait_and_find_elements_by_xpath('//button[.="Join"]', 3)
-    if len(joins) == 0: # no meeting scheduled
-        return
-    joins[-1].click()   # join the latest meeting scheduled i.e if join buttons for 9 A.M and 10 A.M available, will join 10 A.M
-    elem = wait_and_find_element_by_xpath('//*[@id="page-content-wrapper"]/div[1]/div/calling-pre-join-screen/div/div/div[2]/div[1]/div[2]/div/div/section/div[2]/toggle-button[1]/div/button', timeOutDelay)
-    if elem.get_attribute('aria-pressed') == 'true': # turn off camera
-        elem.click()
-    elem = wait_and_find_element_by_xpath('//*[@id="preJoinAudioButton"]/div/button', timeOutDelay)
-    if elem.get_attribute('aria-pressed') == 'true': # turn off microphone
-        elem.click()
-    wait_and_find_element_by_xpath('//button[.="Join now"]', timeOutDelay).click() # join meeting
-    print('Joined the meeting at {}'.format(datetime.now()))
-    sleep(60*5)
-    actions = ActionChains(browser)
-    rosterBtn = wait_and_find_element_by_xpath('//button[@id="roster-button"]', timeOutDelay)
-    actions.move_to_element(rosterBtn).click().perform()
-    numStr = wait_and_find_elements_by_xpath('//span[@class="toggle-number"][@ng-if="::ctrl.enableRosterParticipantsLimit"]')
-    if len(numStr) >= 2:
-        if numStr[1].text[1:-1] != '':
-            maxParticipants = curParticipants = int(numStr[1].text[1:-1])
 
-def checkAndEndOrLeaveOrJoinMeeting():
-    global maxParticipants, curParticipants, minParticipants
-    hangupBtn = wait_and_find_element_by_xpath('//button[@id="hangup-button"]', 2)
-    if hangupBtn != None: # currently in meeting
-        numStr = wait_and_find_elements_by_xpath('//span[@class="toggle-number"][@ng-if="::ctrl.enableRosterParticipantsLimit"]')
-        if len(numStr) >= 2:
-            if numStr[1].text[1:-1] != '':
-                curParticipants = int(numStr[1].text[1:-1])
-            else :
-                actions = ActionChains(browser)
-                actions.move_to_element(wait_and_find_element_by_xpath('//button[@id="roster-button"]', timeOutDelay)).click().perform()
-        maxParticipants = max(maxParticipants, curParticipants)
-        if curParticipants <= minParticipants and curParticipants != 0:   # leaves the meeting automatically for given condition
-            hangupBtn = wait_and_find_element_by_xpath('//button[@id="hangup-button"]', 3)
-            actions = ActionChains(browser)
-            actions.move_to_element(hangupBtn).click().perform()
+def check_and_join_meeting():
+    global curParticipants
+    joins = wait_and_find_elements_by_xpath('//button[.="Join"]', 3)
+    if len(joins) == 0:  # no meeting scheduled
+        return
+    joins[-1].click()  # join the latest meeting scheduled i.e if join buttons for 9, 10 A.M available, will join 10 A.M
+    elem = wait_and_find_element_by_xpath(
+        '//*[@id="page-content-wrapper"]/div[1]/div/calling-pre-join-screen/div/div/div[2]/div[1]/div['
+        '2]/div/div/section/div[2]/toggle-button[1]/div/button')
+    if elem.get_attribute('aria-pressed') == 'true':  # turn off camera
+        elem.click()
+    elem = wait_and_find_element_by_xpath('//*[@id="preJoinAudioButton"]/div/button')
+    if elem.get_attribute('aria-pressed') == 'true':  # turn off microphone
+        elem.click()
+    wait_and_find_element_by_xpath('//button[.="Join now"]').click()  # join meeting
+    print('Joined the meeting at {}'.format(datetime.now()))
+    sleep(60 * 5)
+    browser.execute_script("document.getElementById('roster-button').click()")
+    num_str = wait_and_find_elements_by_xpath(
+        '//span[@class="toggle-number"][@ng-if="::ctrl.enableRosterParticipantsLimit"]')
+    if len(num_str) >= 2:
+        if num_str[1].text[1:-1] != '':
+            curParticipants = int(num_str[1].text[1:-1])
+        else:
+            browser.execute_script("document.getElementById('roster-button').click()")
+
+
+def check_and_end_or_leave_or_join_meeting():
+    global curParticipants, minParticipants
+    hangup_btn = wait_and_find_element_by_xpath('//button[@id="hangup-button"]', 2)
+    if hangup_btn is not None:  # currently in meeting
+        num_str = wait_and_find_elements_by_xpath(
+            '//span[@class="toggle-number"][@ng-if="::ctrl.enableRosterParticipantsLimit"]')
+        if len(num_str) >= 2:
+            if num_str[1].text[1:-1] != '':
+                curParticipants = int(num_str[1].text[1:-1])
+            else:
+                browser.execute_script("document.getElementById('roster-button').click()")
+        if curParticipants <= minParticipants and curParticipants != 0:  # leaves meeting for given condition
+            browser.execute_script("document.getElementById('hangup-button').click()")
             print('Left meeting at {}'.format(datetime.now()))
-            browser.get('https://teams.microsoft.com/_#/calendarv2')    # open calendar tab
+            browser.get(TEAMS_URL)  # open calendar tab
             browser.refresh()
             sleep(5)
-        else :
+        else:
             return
-    else :
-        maxParticipants = curParticipants = 0
-        browser.get('https://teams.microsoft.com/_#/calendarv2')
+    else:
+        curParticipants = 0
+        browser.get(TEAMS_URL)
         browser.refresh()
         sleep(5)
-        checkAndJoinMeeting()
+        check_and_join_meeting()
+
 
 def init():
     global minParticipants
-    browser.get('https://teams.microsoft.com/_#/calendarv2')    # open calendar tab in teams
-    sleep(1)
-    with open(os.path.join(os.path.curdir, 'config.json')) as f:
+    browser.get(TEAMS_URL)  # open calendar tab in teams
+    sleep(sleepDelay)
+    with open('config.json') as f:
         data = json.load(f)
     minParticipants = data['minimumParticipants']
-    wait_and_find_ele_by_id('i0116', timeOutDelay).send_keys(data['username'])      # enter username
-    wait_and_find_ele_by_id('idSIButton9', timeOutDelay).click()                    # click next
-    wait_and_find_ele_by_id('i0118', timeOutDelay).send_keys(data['password'])      # enter password
-    wait_and_find_ele_by_id('idSIButton9', timeOutDelay).click()                    # click next
-    wait_and_find_ele_by_id('idSIButton9', timeOutDelay).click()                    # click yes to stay signed in 
-    wait_and_find_ele_by_link_text('Use the web app instead', timeOutDelay).click() # click use the web app instead link  
-    while wait_and_find_element_by_xpath('//button[@title="Switch your calendar view"]', timeOutDelay) == None: #   wait for calendar tab to completely load
-        sleep(5)
-    while wait_and_find_element_by_xpath('//button[@title="Switch your calendar view"]', timeOutDelay).get_attribute('name') != "Day": # change calender work-week view to day view
-        wait_and_find_element_by_xpath('//button[@title="Switch your calendar view"]', timeOutDelay).click()
-        wait_and_find_element_by_xpath('//button[@name="Day"]', timeOutDelay).click()
-    print('Initialized Succesfully at {}'.format(datetime.now()))
-    checkAndJoinMeeting()
+    wait_and_find_ele_by_id('i0116').send_keys(data['username'])  # enter username
+    wait_and_find_ele_by_id('idSIButton9').click()  # click next
+    wait_and_find_ele_by_id('i0118').send_keys(data['password'])  # enter password
+    wait_and_find_ele_by_id('idSIButton9').click()  # click next
+    wait_and_find_ele_by_id('idSIButton9').click()  # click yes to stay signed in
+    web_ele = wait_and_find_ele_by_link_text('Use the web app instead', 5)
+    if web_ele is not None:
+        web_ele.click()
+    while wait_and_find_element_by_xpath('//button[@title="Switch your calendar view"]') is None:
+        sleep(5)    # wait for calendar tab to completely load
+    while wait_and_find_element_by_xpath('//button[@title="Switch your calendar view"]').get_attribute('name') != "Day":
+        wait_and_find_element_by_xpath('//button[@title="Switch your calendar view"]').click()
+        wait_and_find_element_by_xpath('//button[@name="Day"]').click() # change calender work-week view to day view
+    print('Initialized Successfully at {}'.format(datetime.now()))
+    check_and_join_meeting()
+
 
 def main():
     global browser
@@ -150,12 +157,13 @@ def main():
     else:
         while True:
             try:
-                checkAndEndOrLeaveOrJoinMeeting()
+                check_and_end_or_leave_or_join_meeting()
             except:
                 print('join meeting failed, trying again')
-                browser.get('https://teams.microsoft.com/_#/calendarv2')    # open calendar tab in teams
+                browser.get(TEAMS_URL)  # open calendar tab in teams
             else:
                 sleep(10)
+
 
 if __name__ == "__main__":
     main()
